c0c252bf007b79bbb8f31132a263c32aa0a9aeec
Denny Edy Saputra <ncstealer@gmail.com>
2024-05-15 17:23:55 +0700

    feat: Add order handling functionality
    This commit adds functionality for handling orders. It includes changes to the `orderHandler` function to update the stock of products based on the quantity ordered. It also updates the `transactionHandler` function to correctly handle the payment method. Additionally, the `PlaceOrder` endpoint is modified to use the updated functions and create the necessary database entries for the order and transaction.

--- app/controller/cashier.js
+++ app/controller/cashier.js
@@ -26,26 +26,58 @@ async function orderHandler (user_id, product) {
     product.forEach(async (item) => {
         const product_id = parseInt(item.product_id);
         const quantity = parseInt(item.quantity);
+        let newStock = 0;
 
         orderData.push({
             user_id: user_id,
             transaction_id: transaction_id,
             product_id: product_id,
             quantity: quantity,
-        })
-    });
+        });
 
+        await Database.product.findUnique({
+            where: {
+                product_id: product_id
+            }
+        })
+        .then(product => {
+            if (product.length == 0) {
+                return res.status(404).json({ message: "Product not found" });
+            }
+            
+            if (product.stock < quantity) {
+                return res.status(400).json({ message: "Insufficient stock" });
+            }
+
+            newStock = product.stock - quantity;
+        })
+        .catch(error => {
+            console.log(error);
+            return res.status(500).json({ message: "Error while update stock!" });
+        });
+
+        await Database.product.update({
+            where: {
+                product_id: product_id
+            },
+            data: {
+                stock: newStock
+            }
+        })
+
+    });
     return orderData;
 }
 
 async function transactionHandler(user_id, orderData, amount, payment, placement_order){
     const transaction_id = orderData[0].transaction_id;
 
+    console.log(transaction_id);
     const transactionData = {
         transaction_id: transaction_id,
         user_id: user_id,
         amount: amount,
-        payment: payment,
+        payment_method: payment,
         placement_order: placement_order,
     }
  
@@ -71,18 +103,23 @@ export const PlaceOrder = async (req, res) => {
         return res.status(500).json({ message: "Internal server error" });
     });
 
-    if (placement_order !== "dine-in" && placement_order !== "take-away") {
+    if (placement_order !== "dine-in" && placement_order !== "takeaway") {
         return res.status(400).json({
             message: "Invalid placement order, must dine-in or takeaway"
         });
     }
 
     const orderData = await orderHandler(user_id, product);
-    const transactionData = await transactionHandler(user_id, orderData, payment, placement_order);
+    const transactionData = await transactionHandler(user_id, orderData, amount, payment, placement_order);
+    console.log(transactionData);
     
     try {
-        await Database.transaction.create(transactionData);
-        await Database.order.createMany(orderData);
+        await Database.transaction.create({
+            data: transactionData
+        });
+        await Database.order.createMany({
+            data: orderData
+        });
 
         return res.status(200).json({ message: "Order placed successfully" });
     } catch (error) {
