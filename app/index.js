e7828bb8be2fef387c3d464fe6fd991775523fa3
Denny Edy Saputra <ncstealer@gmail.com>
2024-06-06 23:45:17 +0700

    Changes to be committed: 	modified:   app/index.js

--- app/index.js
+++ app/index.js
@@ -18,5 +18,5 @@ app.use(morgan('dev'));
 app.use('/', Router);
 
 app.listen(process.env.PORT, () => {
-    console.log('Server is running on port 3080');
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
