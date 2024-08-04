8535d3e9ec76fda96f833225ba8c14f3acab47a4
Denny Edy Saputra <ncstealer@gmail.com>
2024-08-05 01:54:13 +0700

    refactor: Update bodyParser configuration in index.js
    The bodyParser configuration in the `app/index.js` file has been updated to increase the limit for JSON and URL-encoded bodies to 50mb. This change ensures that the application can handle larger request bodies and improves the flexibility of the application.

--- app/index.js
+++ app/index.js
@@ -8,8 +8,8 @@ dotenv.config();
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors());
 app.use(express.json());
